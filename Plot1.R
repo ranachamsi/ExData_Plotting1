94071818a476418997f0853945b86cd9ba090266
ranachamsi <rana_quba@hotmail.com>
2014-05-11 23:27:37 +0200

    Add comments to the R files

--- Plot1.R
+++ Plot1.R
@@ -1,9 +1,16 @@
 require ("sqldf")
-Sys.setlocale("LC_TIME","English")
+# I have a french computer so I'm setting the system local to english
+# in order to display day names in english 
+Sys.setlocale("LC_TIME","English" )  
 
+# Reading data file
 hpc = "./household_power_consumption.txt"
+# the SQL statement for selecting these two dates only
 Sql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+# Read the only the needed data
 subData <- read.csv2.sql(hpc, Sql, filter=NULL)
+
+
 
 # creating plot1
 png("plot1.png", width=480, height= 480)
--- Plot3.R
+++ Plot3.R
@@ -1,8 +1,15 @@
 require ("sqldf")
-Sys.setlocale("LC_TIME","English") 
+# I have a french computer so I'm setting the system local to english
+# in order to display day names in english 
+Sys.setlocale("LC_TIME","English" )  
+
+# Reading data file
 hpc = "./household_power_consumption.txt"
+# the SQL statement for selecting these two dates only
 Sql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+# Read the only the needed data
 subData <- read.csv2.sql(hpc, Sql, filter=NULL)
+# Get the data in the proper format
 subData$Date <- as.Date(subData$Date, format = "%d/%m/%Y")
 subData$DateTime <- as.POSIXct(paste(subData$Date, subData$Time))
 
